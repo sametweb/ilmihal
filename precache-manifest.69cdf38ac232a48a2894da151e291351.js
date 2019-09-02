2c94797a24032cdba3468dd489d1d1412c10489f
sametweb <sametmutevellioglu@gmail.com>
2019-09-02 01:45:01 -0700

    Updates

---
+++ precache-manifest.69cdf38ac232a48a2894da151e291351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8396ae8bb6cae85b5fa648e234b017c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c42ad82eef46158d8d8",
+    "url": "/static/js/2.83973903.chunk.js"
+  },
+  {
+    "revision": "092127abe513b805c424",
+    "url": "/static/js/main.9c28b9a0.chunk.js"
+  },
+  {
+    "revision": "0ffd5a3b8bdaa2fc7a20",
+    "url": "/static/js/runtime~main.fd8edf08.js"
+  },
+  {
+    "revision": "6e09593777260a498237efeab29eaaac",
+    "url": "/static/media/paper-bg.6e095937.jpg"
+  }
+]);
