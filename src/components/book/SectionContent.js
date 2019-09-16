b9326ee3debd4b190dd4f45760de7cef9e917829
sametweb <sametmutevellioglu@gmail.com>
2019-09-15 23:46:21 -0700

    GA tracker on each component

--- src/components/book/SectionContent.js
+++ src/components/book/SectionContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactGA from "react-ga";
 import { book as ilmihal } from "../../newSource";
 import slugify from "../../slugify";
 import { Link } from "react-router-dom";
