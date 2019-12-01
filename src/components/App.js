f705f7d461e2626c736e92e48f2218a4af659c9e
sametweb <sametmutevellioglu@gmail.com>
2019-11-30 20:37:29 -0800

    privacy policy for the app

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import "../style.css";
 import ChapterList from "./book/ChapterList";
 import ChapterContent from "./book/ChapterContent";
 import SectionContent from "./book/SectionContent";
+import PrivacyPolicy from "./PrivacyPolicy";
 // import Sitemap from "./Sitemap";
 
 class App extends React.Component {
@@ -15,6 +16,7 @@ class App extends React.Component {
         <BrowserRouter>
           <Route exact path="/" component={ChapterList} />
           {/* <Route exact path="/Sitemap" component={Sitemap} /> */}
+          <Route exact path="/Gizlilik-Politikasi" component={PrivacyPolicy} />
           <Route exact path="/:slug" component={ChapterContent} />
           <Route exact path="/:slug/:sectionSlug" component={SectionContent} />
         </BrowserRouter>
---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <section className="content privacy">
+      <h1>Gizlilik Politikası</h1>
+      <p>
+        ilmihal oku uygulamasının amacı ilmihal bilgisini size en kolay yoldan
+        ulaştırmayı amaçlamaktadır. Uygulamamız telefonunuzdaki hiçbir uygulama
+        ile bağlantısı olmayıp sadece ve sadece kitap işlevi görmektedir.
+      </p>
+    </section>
+  );
+};
+
+export default PrivacyPolicy;
