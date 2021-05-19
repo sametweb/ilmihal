7c00c9d44fea07ac2e604498e9a10ab16ec3ea39
sametweb <sametmutevellioglu@gmail.com>
2021-05-19 02:30:49 -0700

    Footer link added

--- v2/pages/_app.js
+++ v2/pages/_app.js
@@ -3,15 +3,24 @@ import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="wrapper">
-      <Link href="/" passHref>
-        <a>
-          <h2 className="logo-author">ÖMER NASUHİ BİLMEN</h2>
-          <h1 className="logo-title">BÜYÜK İSLAM İLMİHALİ</h1>
-        </a>
-      </Link>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className="wrapper">
+        <Link href="/" passHref>
+          <a>
+            <h2 className="logo-author">ÖMER NASUHİ BİLMEN</h2>
+            <h1 className="logo-title">BÜYÜK İSLAM İLMİHALİ</h1>
+          </a>
+        </Link>
+        <Component {...pageProps} />
+      </div>
+      <a
+        href="https://sametmutevelli.com"
+        target="_blank"
+        style={{ color: "white", marginTop: 20 }}
+      >
+        Designed and coded by Samet Mutevelli
+      </a>
+    </>
   );
 }
 
