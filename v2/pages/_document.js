a93ebcfaddfbaf6e3fca5e835e79ce190b054b17
sametweb <sametmutevellioglu@gmail.com>
2023-04-29 12:05:07 -0700

    Added Analytics tracking code

--- v2/pages/_document.js
+++ v2/pages/_document.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { CssBaseline } from "@nextui-org/react";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,7 +15,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="tr">
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-146893305-1"
+          />
+          <Script>
+            {`    
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-146893305-1');
+            `}
+          </Script>
+
+          {CssBaseline.flush()}
+        </Head>
         <body>
           <Main />
           <NextScript />
