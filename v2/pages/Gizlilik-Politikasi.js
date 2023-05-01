13726271e1bce8f1db5525aca81d14a4e35001cd
sametweb <sametmutevellioglu@gmail.com>
2023-05-01 15:46:28 -0700

    Privacy Policy updated.

--- v2/pages/Gizlilik-Politikasi.js
+++ v2/pages/Gizlilik-Politikasi.js
@@ -6,6 +6,27 @@ const PrivacyPolicy = () => {
         ilmihal oku uygulamasının amacı ilmihal bilgisini size en kolay yoldan
         ulaştırmaktır.
       </p>
+      <p>
+        Uygulamamızın kullanıcı gizliliğine büyük önem verdiğini belirtmek
+        isteriz. Bu nedenle, uygulamamız hiçbir kullanıcı verisi toplamamakta ve
+        paylaşmamaktadır.
+      </p>
+      <p>
+        Uygulamamızda kullanılan kitaplıklar ve SDK'lar, kullanıcı verilerini
+        toplamamakta ve dışarı göndermemektedir.
+      </p>
+      <p>
+        Uygulamamız, sunucularımızdan herhangi bir kullanıcı verisini üçüncü
+        taraflara veya aynı cihazda bulunan başka bir üçüncü taraf uygulamasına
+        aktarmamaktadır.
+      </p>
+      <p>Uygulamamız içerisinde bir web görünümü bulunmamaktadır.</p>
+      <p>
+        Özetle, uygulamamız kullanıcıların gizliliklerine saygı göstermekte ve
+        hiçbir kullanıcı verisi toplamamakta veya paylaşmamaktadır.
+        Kullanıcıların rahatlıkla ve güvenle uygulamamızı kullanabileceğini
+        taahhüt ederiz.
+      </p>
     </section>
   );
 };
